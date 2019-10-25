6caeeb450f11da06b05783f74da219329acfb694
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2019-10-25 13:30:27 -0500

    Added Amplify Auth services

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/** React Router */
+import { Router, Route } from 'react-router-dom'
+
+/** AWS Amplify Components */
+import { Hub, Auth } from 'aws-amplify'
+import { Authenticator } from 'aws-amplify-react'
+
+class App extends React.Component {
+  state = {
+    user: null,
+    userAttributes: null
+  }
+
+  componentDidMount = () => {
+    this.getUserData()
+    Hub.listen('auth', this, 'onHubCapsule')
+  }
+
+  getUserData = async () => {
+    const user = await Auth.currentAuthenticatedUser()
+    user ? this.setState({ user }, () => this.getUserAttributes(this.state.user)) : this.setState({ user: null })
+  }
+
+  getUserAttributes = async authUserData => {
+    const attributesArr = await Auth.userAttributes(authUserData)
+    const attributesObj = Auth.attributesToObject(attributesArr)
+    this.setState({ userAttributes: attributesObj })
+  }
+
+  handleSignOut = async () => {
+    try {
+      await Auth.signOut()
+    } catch (err) {
+      alert("An error has occured while trying to sign out")
+    }
+  }
+
+  onHubCapsule = async (capsule) => {
+    switch (capsule.payload.event) {
+      case 'signIn':
+        console.log('The user signed in')
+        break
+      case 'signUp':
+        console.log('An user signed up')
+        break
+      case 'signOut':
+        console.log('The user signed out')
+        break
+      case 'signIn_failure':
+        console.log('The user failed to sign in')
+        break
+      default:
+        console.log(capsule.payload.event)
+    }
+  }
+
+  render() {
+    const {user, userAttributes} = this.state
+    return !user ? <Authenticator/> : (
+      <h1> Logged </h1>
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+/** AWS Amplify setup */
+import Amplify from 'aws-amplify'
+import awsmobile from './aws-exports'
+
+/** Amplify Configuration */
+Amplify.configure(awsmobile)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
