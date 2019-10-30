acb7f523558497d52ab0435d9655f6257f3eedf9
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2019-10-30 13:58:51 -0600

    Donors search done

--- src/components/DonorSearch.js
+++ src/components/DonorSearch.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Form, Input, Button } from 'element-react'
 
 class DonorSearch extends React.Component {
     render () {
         return (
-            <h1> Donor Search</h1>
+           <div className="market-header">
+               {/** Title */}
+               <h1> Donor Search</h1>
+               {/** Elastic Search */}
+               <Form
+                inline={true}
+                onSubmit={this.props.handleSearch}>
+                    <Form.Item>
+                        <Input
+                            placeholder="Search donors..."
+                            icon="circle-cross"
+                            onIconClick={this.props.handleClearSearch}
+                            onChange={this.props.handleSearchChange}
+                            value={this.props.searchTerm}/>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button
+                            type="info"
+                            icon="search"
+                            onClick={this.props.handleSearch}
+                            loading={this.props.isSearching}>
+                                Search
+                            </Button>
+                    </Form.Item>
+                </Form>
+           </div>
         )
     }
 }
