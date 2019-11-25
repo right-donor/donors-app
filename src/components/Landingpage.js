5177095edf46734e397c23d71bb2c6950ad4ecde
DafLec <dafnelecona@gmail.com>
2019-11-25 10:40:37 -0600

    Centered

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -1,48 +1,68 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import CardHeader from '@material-ui/core/CardHeader';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Collapse from '@material-ui/core/Collapse';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+
 import App from '../App';
 import {Router} from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 import clsx from 'clsx';
 
-/** Material-UI Elements */
-import Header from "../useful/Header/Header.js";
-import Button from "../useful/CustomButtons/Button.js";
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-export const history = createBrowserHistory()
 const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
+  icon: {
     marginRight: theme.spacing(2),
   },
-  title: {
-    flexGrow: 1,
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
   },
   card: {
-    maxWidth: 345,
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
   },
-  media: {
-    height: 0,
+  cardMedia: {
     paddingTop: '56.25%', // 16:9
   },
+  cardContent: {
+    flexGrow: 1,
+  },
+  
   expand: {
     transform: 'rotate(0deg)',
     marginLeft: 'auto',
@@ -53,30 +73,60 @@ const useStyles = makeStyles(theme => ({
   expandOpen: {
     transform: 'rotate(180deg)',
   },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
 }));
 
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-export default function Landingpage() {
-	const classes = useStyles();
-	const [expanded, setExpanded] = React.useState(false);
+export default function Album() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
 
-	const handleExpandClick = () => {
-    setExpanded(!expanded);
-	};
+  const handleExpandClick = () => {
+	setExpanded(!expanded);
+  };
 
-    return (
-		<wrapper>
-		  <AppBar position="static">
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+		<AppBar position="static">
 			<Toolbar>
 			  <Typography variant="h6" className={classes.title}>
 				Right Donor
 			  </Typography>
-			  <Button color="inherit">Login</Button>
 			</Toolbar>
-		  </AppBar>
-		  <section>
-			<h1>GRAPHIC DESIGN IS MY PASSION</h1>
-			<Card className={classes.card}>
+		 </AppBar>
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              GRAPHIC DESIGN IS MY PASSION
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin accumsan nec est quis pretium.
+			  Curabitur pretium eget ex nec iaculis. Pellentesque in porta dolor, eget eleifend lorem. Pellentesque in semper leo. 
+			  Ut molestie augue nec ullamcorper fringilla. Maecenas nisl tellus, varius a laoreet et, porttitor at quam.
+            </Typography>
+            <div className={classes.heroButtons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Login
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            <Card className={classes.card}>
 			  <CardHeader
 				title="¡Atrevete!"
 				subheader="Beneficios de ser donante"
@@ -132,7 +182,20 @@ export default function Landingpage() {
 				</CardContent>
 			  </Collapse>
 			</Card>
-		  </section>
-		</wrapper>
-    )
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright />
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
+  );
 }
