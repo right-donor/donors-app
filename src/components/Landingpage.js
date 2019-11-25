2d3c1873c0c136af7a089cf9e5bf81135a56809e
DafLec <dafnelecona@gmail.com>
2019-11-25 11:09:43 -0600

    unnessary import deleted

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
 import CardHeader from '@material-ui/core/CardHeader';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
