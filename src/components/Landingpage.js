032d39e843ae36c9784304634e84819eef5b0643
Abraham Silva <ab_a28@hotmail.com>
2019-11-25 20:20:53 -0600

    hero improvements

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -16,16 +16,18 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import App from '../App';
 import {Router} from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 import clsx from 'clsx';
+import BackgroundHero from '../assets/img/donor-2.jpeg';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {'Copyright ï¿½ '}
       <Link color="inherit" href="https://material-ui.com/">
         Your Website
       </Link>{' '}
@@ -35,16 +37,42 @@ function Copyright() {
   );
 }
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      500:"#833741"
+    } ,
+  },
+});
+
 const useStyles = makeStyles(theme => ({
+  banner: {
+    width: '60%',
+    color: '#7E353E',
+    fontWeight:'500',
+    marginLeft:'auto'
+  },
+  bannerText: {
+    width: '60%',
+    marginLeft:'auto'
+  },
+  toolbar: {
+    backgroundColor: "#833741"
+  },
   icon: {
     marginRight: theme.spacing(2),
   },
   heroContent: {
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(8, 0, 6),
+    height: '75vh'
   },
   heroButtons: {
     marginTop: theme.spacing(4),
+    display: 'flex',
+    justifyContent:'center',
+    width:'60%',
+    marginLeft:'auto'
   },
   cardGrid: {
     paddingTop: theme.spacing(8),
@@ -93,7 +121,7 @@ export default function Album() {
     <React.Fragment>
       <CssBaseline />
 		<AppBar position="static">
-			<Toolbar>
+			<Toolbar className={classes.toolbar}>
 			  <Typography variant="h6" className={classes.title}>
 				Right Donor
 			  </Typography>
@@ -101,24 +129,26 @@ export default function Album() {
 		 </AppBar>
       <main>
         {/* Hero unit */}
-        <div className={classes.heroContent}>
-          <Container maxWidth="sm">
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-              GRAPHIC DESIGN IS MY PASSION
+        <div className={classes.heroContent} 
+        style={{backgroundImage:"url(" + BackgroundHero + ")",backgroundSize: 'cover', 
+                backgroundPosition: 'center center',
+                backgroundRepeat: 'no-repeat'}}>
+          <Container >
+            <Typography component="h1" variant="h2" align="center" className={classes.banner} gutterBottom>
+              RIGHT DONOR
             </Typography>
-            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+            <Typography variant="h5" align="center" color="textSecondary" className={classes.bannerText}paragraph>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin accumsan nec est quis pretium.
 			  Curabitur pretium eget ex nec iaculis. Pellentesque in porta dolor, eget eleifend lorem. Pellentesque in semper leo. 
 			  Ut molestie augue nec ullamcorper fringilla. Maecenas nisl tellus, varius a laoreet et, porttitor at quam.
             </Typography>
             <div className={classes.heroButtons}>
-              <Grid container spacing={2} justify="center">
-                <Grid item>
-                  <Button variant="outlined" color="primary">
-                    Login
-                  </Button>
-                </Grid>
-              </Grid>
+              <MuiThemeProvider theme={theme}>
+                <Button variant="contained" color='primary'>
+                  Login
+                </Button> 
+              </MuiThemeProvider>              
+              
             </div>
           </Container>
         </div>
@@ -126,61 +156,117 @@ export default function Album() {
           {/* End hero unit */}
           <Grid container spacing={4}>
             <Card className={classes.card}>
-			  <CardHeader
-				title="¡Atrevete!"
-				subheader="Beneficios de ser donante"
-			  />
-			  <CardMedia
-				className={classes.media}
-				image="/static/images/cards/paella.jpg"
-				title="Donante"
-			  />
-			  <CardContent>
-				<Typography variant="body2" color="textSecondary" component="p">
-				  This impressive paella is a perfect party dish and a fun meal to cook together with your
-				  guests. Add 1 cup of frozen peas along with the mussels, if you like.
-				</Typography>
-			  </CardContent>
-			  <CardActions disableSpacing>
-				<IconButton
-				  className={clsx(classes.expand, {
-					[classes.expandOpen]: expanded,
-				  })}
-				  onClick={handleExpandClick}
-				  aria-expanded={expanded}
-				  aria-label="show more"
-				>
-				  <ExpandMoreIcon />
-				</IconButton>
-			  </CardActions>
-			  <Collapse in={expanded} timeout="auto" unmountOnExit>
-				<CardContent>
-				  <Typography paragraph>Method:</Typography>
-				  <Typography paragraph>
-					Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-					minutes.
-				  </Typography>
-				  <Typography paragraph>
-					Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-					heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-					browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-					and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-					pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-					saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-				  </Typography>
-				  <Typography paragraph>
-					Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-					without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-					medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-					again without stirring, until mussels have opened and rice is just tender, 5 to 7
-					minutes more. (Discard any mussels that don’t open.)
-				  </Typography>
-				  <Typography>
-					Set aside off of the heat to let rest for 10 minutes, and then serve.
-				  </Typography>
-				</CardContent>
-			  </Collapse>
-			</Card>
+              <CardHeader
+                title="ï¿½Atrevete!"
+                subheader="Beneficios de ser donante"
+              />
+              <CardMedia
+                className={classes.media}
+                image="/static/images/cards/paella.jpg"
+                title="Donante"
+              />
+              <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  This impressive paella is a perfect party dish and a fun meal to cook together with your
+                  guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                </Typography>
+              </CardContent>
+              <CardActions disableSpacing>
+                <IconButton
+                  className={clsx(classes.expand, {
+                  [classes.expandOpen]: expanded,
+                  })}
+                  onClick={handleExpandClick}
+                  aria-expanded={expanded}
+                  aria-label="show more"
+                >
+                <ExpandMoreIcon />
+                </IconButton>
+              </CardActions>
+              <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                  <Typography paragraph>Method:</Typography>
+                  <Typography paragraph>
+                  Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+                  minutes.
+                  </Typography>
+                  <Typography paragraph>
+                  Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+                  heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+                  browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
+                  and chorizo in the pan. Add pimentï¿½n, bay leaves, garlic, tomatoes, onion, salt and
+                  pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
+                  saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+                  </Typography>
+                  <Typography paragraph>
+                  Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+                  without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+                  medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
+                  again without stirring, until mussels have opened and rice is just tender, 5 to 7
+                  minutes more. (Discard any mussels that donï¿½t open.)
+                  </Typography>
+                  <Typography>
+                  Set aside off of the heat to let rest for 10 minutes, and then serve.
+                  </Typography>
+                </CardContent>
+              </Collapse>
+		  	    </Card>
+            <Card className={classes.card}>
+            <CardHeader
+                title="ï¿½Atrevete!"
+                subheader="Beneficios de ser donante"
+              />
+              <CardMedia
+                className={classes.media}
+                image="/static/images/cards/paella.jpg"
+                title="Donante"
+              />
+              <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  This impressive paella is a perfect party dish and a fun meal to cook together with your
+                  guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                </Typography>
+              </CardContent>
+              <CardActions disableSpacing>
+                <IconButton
+                  className={clsx(classes.expand, {
+                  [classes.expandOpen]: expanded,
+                  })}
+                  onClick={handleExpandClick}
+                  aria-expanded={expanded}
+                  aria-label="show more"
+                >
+                <ExpandMoreIcon />
+                </IconButton>
+              </CardActions>
+              <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                  <Typography paragraph>Method:</Typography>
+                  <Typography paragraph>
+                  Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+                  minutes.
+                  </Typography>
+                  <Typography paragraph>
+                  Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+                  heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+                  browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
+                  and chorizo in the pan. Add pimentï¿½n, bay leaves, garlic, tomatoes, onion, salt and
+                  pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
+                  saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+                  </Typography>
+                  <Typography paragraph>
+                  Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+                  without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+                  medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
+                  again without stirring, until mussels have opened and rice is just tender, 5 to 7
+                  minutes more. (Discard any mussels that donï¿½t open.)
+                  </Typography>
+                  <Typography>
+                  Set aside off of the heat to let rest for 10 minutes, and then serve.
+                  </Typography>
+                </CardContent>
+              </Collapse>
+            </Card>
           </Grid>
         </Container>
       </main>
